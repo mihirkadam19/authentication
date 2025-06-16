da660923ca399a9fff11898615adc4c001cc20b2
mihirkadam19 <mihirkadam85@yahoo.in>
2025-06-16 12:43:09 -0400

    enabled cors

--- backend/src/server.js
+++ backend/src/server.js
@@ -2,12 +2,17 @@ import express from 'express';
 import { connectDB } from './db/connectDB.js';
 import authRoutes from './routes/auth.route.js';
 import cookieParser from 'cookie-parser'; 
+import cors from 'cors';
 
 // config app   
 const app = express();
 
 // env variables
 const PORT = process.env.PORT || 5000;
+
+// enable cors
+app.use(cors({ origin: process.env.FRONTEND_URL, credentials: true }));
+
 
 // middleware
 app.use(express.json());        //  allows us to parse incoming req
