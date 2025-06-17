73f8a1b10c7deed5282563597ce9ad9125f21e99
mihirkadam19 <mihirkadam85@yahoo.in>
2025-06-17 00:46:10 -0400

    prod cookie bug

--- backend/src/utils/genTokenAndSetCookie.js
+++ backend/src/utils/genTokenAndSetCookie.js
@@ -8,7 +8,7 @@ export const generateTokenAndSetCookie = (res, userId) => {
         httpOnly: true, // prevent XSS attacks by preventing access to the cookie from JavaScript
         secure: process.env.NODE_ENV === "production", // only send cookie over HTTPS in production
         maxAge: 4 * 24 * 60 * 60 * 1000, // 4 days
-        sameSite: "strict", // prevent CSRF attacks
+        sameSite: process.env.NODE_ENV === "production" ? "none" : "strict"
     });
 
     return token;
